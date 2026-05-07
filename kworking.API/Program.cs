0685c951320684b12f2831eea5c466209d7f2801
Dn <rusalovskaaad@gmail.com>
2026-05-07 22:35:01 +0300

    добавлен AuthController с методом POST /api/auth/login для аутентификации пользователя настроен, Program.cs: подключены DbContext, Swagger, CORS, сериализация enum в строки

--- kworking.API/Program.cs
+++ kworking.API/Program.cs
@@ -1,29 +1,34 @@
 using Microsoft.EntityFrameworkCore;
 using kworking.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
-// Вместо UseSqlServer используйте UseNpgsql
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
+
 builder.Services.AddDbContext<KworkingDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
+app.UseCors();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
