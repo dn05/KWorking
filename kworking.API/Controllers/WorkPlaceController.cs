f81fd93fa4924d78ad3231a83cbf37e8e7fb6626
Dn <rusalovskaaad@gmail.com>
2026-05-07 16:18:52 +0300

    добавлен метод GetById в WorkPlaceController для получения рабочего места по ID

--- kworking.API/Controllers/WorkPlaceController.cs
+++ kworking.API/Controllers/WorkPlaceController.cs
@@ -17,10 +17,19 @@ public class WorkPlaceController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<User>>> GetAll()
+    public async Task<ActionResult<List<WorkPlace>>> GetAll()
     {
         var WorkPlaces = await _dbContext.WorkPlaces.ToListAsync();
         return Ok(WorkPlaces);
     }
-        
+    [HttpGet("{id}")]
+    public async Task<ActionResult<WorkPlace>> GetById(int id)
+    {
+        var workPlace = await _dbContext.WorkPlaces.FindAsync(id);
+        if (workPlace == null)
+        {
+            return NotFound($"Рабочее место с ID {id} не найдено");
+        }
+        return Ok(workPlace);
+    }
 }
